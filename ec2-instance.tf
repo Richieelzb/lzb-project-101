42b626c5b49f7fda9b160f2d2e14050a6518f89b
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-16 15:03:51 +0200

    add sg

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "docker-instance" {
   ami             = data.aws_ami.amzn2023_ami.id
-  instance_type   = var.instance_type["dev"]
+  instance_type   = var.instance-type-list[0]
   subnet_id       = module.vpc1.public_subnets[0]
   key_name        = var.key-pair
   user_data       = file("${path.module}/docker-install.sh")
