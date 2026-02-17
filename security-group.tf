607f7c6235488d7ddaf0d4836e40656c5710cf63
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-17 18:51:12 +0200

    changing sg rules names

--- security-group.tf
+++ security-group.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "sg-docker" {
   name = "${local.Name}-sg-public"
 }
 
-resource "aws_security_group_rule" "docker-sg" {
+resource "aws_security_group_rule" "port-80" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
@@ -12,7 +12,7 @@ resource "aws_security_group_rule" "docker-sg" {
   security_group_id = aws_security_group.sg-docker.id
 }
 
-resource "aws_security_group_rule" "docker-sg" {
+resource "aws_security_group_rule" "port-22" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
@@ -21,7 +21,7 @@ resource "aws_security_group_rule" "docker-sg" {
   security_group_id = aws_security_group.sg-docker.id
 }
 
-resource "aws_security_group_rule" "docker-sg" {
+resource "aws_security_group_rule" "port-8888" {
   type              = "ingress"
   from_port         = 8888
   to_port           = 8888
@@ -30,7 +30,7 @@ resource "aws_security_group_rule" "docker-sg" {
   security_group_id = aws_security_group.sg-docker.id
 }
 
-resource "aws_security_group_rule" "docker-sg" {
+resource "aws_security_group_rule" "allow-all" {
   type              = "egress"
   from_port         = -1
   to_port           = -1
