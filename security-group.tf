2f37ea47c69895fa917c89a3d8ecd00caed2190d
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-17 18:45:13 +0200

    changing the sg

--- security-group.tf
+++ security-group.tf
@@ -4,40 +4,40 @@ resource "aws_security_group" "sg-docker" {
 }
 
 resource "aws_security_group_rule" "docker-sg" {
-  type            = "ingress"
-  from_port       = 80
-  to_port         = 80
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg-docker.id
-  }
+}
 
 resource "aws_security_group_rule" "docker-sg" {
-  type            = "ingress"
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg-docker.id
-  }
+}
 
 resource "aws_security_group_rule" "docker-sg" {
-  type            = "ingress"
-  from_port       = 8888
-  to_port         = 8888
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 8888
+  to_port           = 8888
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg-docker.id
-  }
+}
 
 resource "aws_security_group_rule" "docker-sg" {
-  type            = "egress"
-  from_port       = -1
-  to_port         = -1
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
+  type              = "egress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg-docker.id
-  }
+}
 
-  
-  
+
+
