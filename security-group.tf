56df46036afd8dc9a90b8493667c50ca0bbb402f
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-17 19:06:37 +0200

    changing sg rules names

--- security-group.tf
+++ security-group.tf
@@ -1,6 +1,7 @@
 /////////////////VPC 1 SG/////////////////
 resource "aws_security_group" "sg-docker" {
   name = "${local.Name}-sg-public"
+  vpc_id      = module.vpc1.vpc_id
 }
 
 resource "aws_security_group_rule" "port-80" {
@@ -32,9 +33,9 @@ resource "aws_security_group_rule" "port-8888" {
 
 resource "aws_security_group_rule" "allow-all" {
   type              = "egress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg-docker.id
 }
