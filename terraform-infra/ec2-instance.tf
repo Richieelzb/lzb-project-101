dd93a113d3a036e32c4d06c3111d60e2e982bac2
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-18 15:16:13 +0200

    add

--- terraform-infra/ec2-instance.tf
+++ terraform-infra/ec2-instance.tf
@@ -1,12 +1,12 @@
-# resource "aws_instance" "docker-instance" {
-#   ami             = data.aws_ami.amzn2023_ami.id
-#   instance_type   = var.instance-type-list[0]
-#   subnet_id       = module.vpc1.public_subnets[0]
-#   key_name        = var.key-pair
-#   user_data       = file("${path.module}/docker-install.sh")
-#   security_groups = [aws_security_group.sg-docker.id]
+resource "aws_instance" "docker-instance" {
+  ami             = data.aws_ami.amzn2023_ami.id
+  instance_type   = var.instance-type-list[0]
+  subnet_id       = module.vpc1.public_subnets[0]
+  key_name        = var.key-pair
+  user_data       = file("${path.module}/docker-install.sh")
+  security_groups = [aws_security_group.sg-docker.id]
 
-#   tags = {
-#     Name = "${local.Name}-docker"
-#   }
-# }
+  tags = {
+    Name = "${local.Name}-docker"
+  }
+}
