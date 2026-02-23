8001487fee1bc3e7fb6c1721cb94eadafcf22624
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-23 16:18:44 +0200

    installing docker compose

--- terraform-infra/docker-install.sh
+++ terraform-infra/docker-install.sh
@@ -5,3 +5,8 @@ sudo dnf install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
+
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -O docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/lib/docker/cli-plugins/docker-compose
