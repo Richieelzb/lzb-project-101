01713606b811c66b04021f37d28c39f0eba446cf
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-24 12:21:40 +0200

    create port 8888

--- terraform-infra/security-group.tf
+++ terraform-infra/security-group.tf
@@ -31,6 +31,15 @@ resource "aws_security_group_rule" "port-8888" {
   security_group_id = aws_security_group.sg-docker.id
 }
 
+resource "aws_security_group_rule" "port-8888" {
+  type              = "ingress"
+  from_port         = 8888
+  to_port           = 8888
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg-docker.id
+}
+
 resource "aws_security_group_rule" "allow-all" {
   type              = "egress"
   from_port         = 0
