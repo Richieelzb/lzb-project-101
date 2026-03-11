7ce092ef5e53d57cafcdaf591de568ba16954ec9
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-03-11 21:07:53 +0200

    creating 1

--- terraform-infra/eip.tf
+++ terraform-infra/eip.tf
@@ -1,6 +1,6 @@
 resource "aws_eip" "bastion-eip" {
-  depends_on = [aws_instance.main-instance, module.vpc1]
-  instance   = aws_instance.main-instance.id
+  depends_on = [aws_instance.bastion-instance, module.vpc1]
+  instance   = aws_instance.bastion-instance.id
   // vpc = true
   tags = local.common_tags
 }
