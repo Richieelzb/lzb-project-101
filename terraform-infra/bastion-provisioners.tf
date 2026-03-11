1b0074f61c1fa53cf3973701b68dabc52ebd0e5e
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-03-11 14:53:05 +0200

    creating provisioners2

--- terraform-infra/bastion-provisioners.tf
+++ terraform-infra/bastion-provisioners.tf
@@ -1,34 +1,34 @@
 resource "null_resource" "ec2_copy_manifests" {
-   depends_on = [aws_instance.main-instance, module.eks ]
+  depends_on = [aws_instance.main-instance, module.eks]
 
-   triggers = {
-      # Re-run when any file under eks-resources changes
-      manifest_hash = sha1(join(",", [
-        for f in fileset(path.module, "eks-resources/**") : filesha1("${path.module}/${f}")
-      ]))
-      # Re-run when cluster details change
-      cluster_name = module.eks.cluster_name
-      cluster_arn  = module.eks.cluster_arn
-     }
+  triggers = {
+    # Re-run when any file under eks-resources changes
+    manifest_hash = sha1(join(",", [
+      for f in fileset(path.module, "eks-resources/**") : filesha1("${path.module}/${f}")
+    ]))
+    # Re-run when cluster details change
+    cluster_name = module.eks.cluster_name
+    cluster_arn  = module.eks.cluster_arn
+  }
 
-   connection {
-     type = "ssh"
-     host = aws_eip.bastion-eip.public_ip
-     user = "ec2-user"
-     password = ""
-     private_key = file("${path.module}/../lupfumo-key-mumbai.pem")
-   }
+  connection {
+    type        = "ssh"
+    host        = aws_eip.bastion-eip.public_ip
+    user        = "ec2-user"
+    password    = ""
+    private_key = file("${path.module}/../lupfumo-key-mumbai.pem")
+  }
 
-   provisioner "file" {
+  provisioner "file" {
     source      = "eks-resources/."
     destination = "/tmp/eks-resources/."
-   }
+  }
 
-   provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
-    # Update kubeconfig for this user
+      # Update kubeconfig for this user
       "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
     ]
-   }
+  }
 
 }
