10efd33c534c587aeae9dcea815f5002d452b451
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-03-13 00:06:31 +0200

    recreating ec2 instance

--- terraform-infra/bastion-provisioners.tf
+++ terraform-infra/bastion-provisioners.tf
@@ -1,39 +1,39 @@
-# resource "null_resource" "ec2_copy_manifests" {
-#   depends_on = [aws_instance.bastion-instance, module.eks]
+resource "null_resource" "ec2_copy_manifests" {
+  depends_on = [aws_instance.bastion-instance, module.eks]
 
-#   triggers = {
-#     # Re-run when any file under eks-resources changes
-#     manifest_hash = sha1(join(",", [
-#       for f in fileset(path.module, "manifests/**") : filesha1("${path.module}/${f}")
-#     ]))
-#     # Re-run when cluster details change
-#     cluster_name = module.eks.cluster_name
-#     cluster_arn  = module.eks.cluster_arn
-#   }
+  triggers = {
+    # Re-run when any file under eks-resources changes
+    manifest_hash = sha1(join(",", [
+      for f in fileset(path.module, "manifests/**") : filesha1("${path.module}/${f}")
+    ]))
+    # Re-run when cluster details change
+    cluster_name = module.eks.cluster_name
+    cluster_arn  = module.eks.cluster_arn
+  }
 
-#   connection {
-#     type        = "ssh"
-#     host        = aws_eip.bastion-eip.public_ip
-#     user        = "ec2-user"
-#     password    = ""
-#     private_key = file("${path.module}/../lupfumo-key-mumbai.pem")
-#   }
+  connection {
+    type        = "ssh"
+    host        = aws_eip.bastion-eip.public_ip
+    user        = "ec2-user"
+    password    = ""
+    private_key = file("${path.module}/../lupfumo-key-mumbai.pem")
+  }
 
-#   provisioner "file" {
-#     source      = data.archive_file.manifests_zip.output_path
-#     destination = "/tmp/manifests.zip"
-#   }
+  provisioner "file" {
+    source      = data.archive_file.manifests_zip.output_path
+    destination = "/tmp/manifests.zip"
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "unzip /tmp/manifests.zip -d /tmp/manifests"
-#     ]
-#   }
-#   provisioner "remote-exec" {
-#     inline = [
-#       # Update kubeconfig for this user
-#       "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
-#     ]
-#   }
+  provisioner "remote-exec" {
+    inline = [
+      "unzip /tmp/manifests.zip -d /tmp/manifests"
+    ]
+  }
+  provisioner "remote-exec" {
+    inline = [
+      # Update kubeconfig for this user
+      "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
+    ]
+  }
 
-# }
+}
