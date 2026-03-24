8dddb6ddde778078350fd845d57b56b981a9bb02
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-03-24 10:41:24 +0200

    updating helm script

--- terraform-infra/bash-scripts/helm-install.sh
+++ terraform-infra/bash-scripts/helm-install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
 helm repo add aws-secrets-manager https://aws.github.io/secrets-store-csi-driver-provider-aws
 helm repo update
