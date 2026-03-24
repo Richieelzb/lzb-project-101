7af187581570f060395284a4185208d8973968c2
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-03-24 23:44:07 +0200

    create 5

--- terraform-infra/iam-role-access.tf
+++ terraform-infra/iam-role-access.tf
@@ -75,7 +75,7 @@ resource "aws_iam_role" "ebs_csi_driver_role" {
 
 resource "aws_iam_role_policy_attachment" "pod_identity_ebs_csi_policy" {
   role       = aws_iam_role.ebs_csi_driver_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "pod_identity_secret_access" {
