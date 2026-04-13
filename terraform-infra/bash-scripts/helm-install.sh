6fc681fb3e7a56520a42612466168872ab3d2d59
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-04-13 12:10:33 +0200

    updating bash scripts

--- terraform-infra/bash-scripts/helm-install.sh
+++ terraform-infra/bash-scripts/helm-install.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
-set -xe
 
-echo "Installing Helm 3..."
-
-# Install Helm
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-# Ensure PATH is updated for all sessions
-echo 'export PATH=/usr/local/bin:$PATH' >> /etc/profile
-export PATH=/usr/local/bin:$PATH
-
-echo "Adding Helm repositories..."
-
-# Add CSI Drivers repos
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
 helm repo add aws-secrets-manager https://aws.github.io/secrets-store-csi-driver-provider-aws
-
-# Update repos
 helm repo update
-
-echo "Helm installation complete."
--- terraform-infra/bash-scripts/secret-store-driver.sh
+++ terraform-infra/bash-scripts/secret-store-driver.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-set -xe
-
-echo "Waiting for Kubernetes API..."
-until kubectl get nodes; do
-  echo "Kubernetes not ready yet... retrying in 10 seconds"
-  sleep 10
-done
-
-echo "Kubernetes is ready. Installing CSI Secrets Store driver..."
-
 helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver --namespace kube-system
 kubectl apply -n kube-system \
   -f https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml
