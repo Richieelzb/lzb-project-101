294673087a0d0356b4ce9d0a79a05024f4caf04e
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-04-14 19:14:55 +0200

    get busy

--- terraform-infra/ebs-csi-driver-agent.tf
+++ terraform-infra/ebs-csi-driver-agent.tf
@@ -1,8 +1,8 @@
 resource "aws_eks_addon" "ebs_csi_driver" {
-  depends_on    = [module.eks]
-  cluster_name  = module.eks.cluster_name
-  addon_name    = "aws-ebs-csi-driver"
-  addon_version = "v1.58.0-eksbuild.1"
+  depends_on               = [module.eks]
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.58.0-eksbuild.1"
   service_account_role_arn = aws_iam_role.ebs_csi_driver_role.arn
   //resolve_conflicts = "OVERWRITE"
 }
