76748ff24dcea429f2afc90e576c9f969b4c5614
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-04-15 16:58:05 +0200

    create ekd

--- terraform-infra/pod-identity-agent.tf
+++ terraform-infra/pod-identity-agent.tf
@@ -22,10 +22,10 @@ resource "aws_eks_pod_identity_association" "aws-secret-access" {
   role_arn        = aws_iam_role.secret_access_role.arn
 }
 
-resource "aws_eks_pod_identity_association" "aws-ebs-csi-driver" {
-  depends_on      = [aws_instance.bastion-instance, module.eks, null_resource.ec2_copy_manifests]
-  cluster_name    = module.eks.cluster_name
-  namespace       = "kube-system"
-  service_account = "ebs-csi-controller-sa"
-  role_arn        = aws_iam_role.ebs_csi_driver_role.arn
-}
+# resource "aws_eks_pod_identity_association" "aws-ebs-csi-driver" {
+#   depends_on      = [aws_instance.bastion-instance, module.eks, null_resource.ec2_copy_manifests]
+#   cluster_name    = module.eks.cluster_name
+#   namespace       = "kube-system"
+#   service_account = "ebs-csi-controller-sa"
+#   role_arn        = aws_iam_role.ebs_csi_driver_role.arn
+# }
